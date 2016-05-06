4e08485d9c53ea431a764439ff17f4d36c39bc98
ratikesh <ratikesh.limetray@gmail.com>
2016-05-06 13:54:59 +0545

    Bash Script for declare

--- type_cast.sh
+++ type_cast.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-a=2334
+a="2334"
 
 let "a+=1"
 
+## this  is the auto type casting that variable string a get added to"
+echo "Previous value of a=$a".
+
+#declare -i  is used to typecast the variable to  int
+
+declare -i a
+
 echo "a=$a"
 
+b="shivam"
 
+d=${b/sh/23}
+
+echo $d
